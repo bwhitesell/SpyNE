19d15d62517d965b710b12aa9f81689bff272392
bwhitesell <whitesell.ben@gmail.com>
2019-07-02 14:15:08 -0700

    restructured project, added new files

--- autodiff/example.py
+++ autodiff/example.py
@@ -3,7 +3,7 @@ import numpy as np
 from operations.arithmetic import TensorMultiply, TensorAddition
 from operations.activations import TensorReLU
 from variables.variables import Tensor, TensorConst
-from differentiation.derivatives import BackwardsPass
+from autodiff.derivatives import BackwardsPass
 
 
 def neural_network(x):
