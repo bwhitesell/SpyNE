a35a33ff60f1b5b2a6bbda112b3156f7b931d611
Ben Whitesell <whitesell.ben@gmail.com>
2019-07-05 21:08:42 -0500

    implemented ADAM and RMSProp optimization

--- example.py
+++ example.py
@@ -4,26 +4,17 @@ from models.graphs import NeuralNetwork
 from layers import FullyConnectedLayer
 
 
-x = np.array([[.1, .5, 3, 2, 2, 5, 4],
-              [.1, .5, 3, 2, 2, 5, 4],
-              [.1, .5, 3, 2, 2, 5, 4],
-              [.1, .5, 3, 2, 2, 5, 4]])
-y = np.array([[500],
-              [500],
-              [500],
-              [500]])
+x = np.random.random((5000, 224))
+y = x.sum(axis=1)
 
 
 nn = NeuralNetwork()
-nn.add_layer(FullyConnectedLayer(neurons=5000, activation='relu'))
-nn.add_layer(FullyConnectedLayer(neurons=2500, activation='relu'))
-nn.add_layer(FullyConnectedLayer(neurons=1000, activation='relu'))
-nn.add_layer(FullyConnectedLayer(neurons=500, activation='relu'))
+nn.add_layer(FullyConnectedLayer(neurons=50, activation='relu'))
 nn.add_layer(FullyConnectedLayer(neurons=1, activation='linear'))
 
 
 print('Learning relationship...')
 
-nn.fit(x, y, batch_size=1, epochs=30, learning_rate=.00000000000001, optimizer='momentum')
+nn.fit(x, y, batch_size=1000, epochs=5, learning_rate=.5, optimizer='adam')
 
 
--- models/optimizers.py
+++ models/optimizers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from autodiff.differentiation.derivatives import BackwardsPass
 from autodiff.variables.variables import Tensor
 
@@ -72,17 +74,57 @@ class MomentumOptimizer(BaseOptimizer):
             else:
                 self.m[var_uid] = -self.learning_rate * gradient[var_uid]
 
-            nn.vars[var_uid].value += self.m[var_uid]
-
-
+            nn.vars[var_uid].value = nn.vars[var_uid].value + self.m[var_uid]
 
 
 class RMSPropOptimizer(BaseOptimizer):
-    pass
+    m = {}
+
+    def __init__(self, loss, learning_rate, beta=0.9, epsilon=1e-10):
+        super().__init__(loss, learning_rate)
+        self.beta = beta
+        self.epsilon = epsilon
+
+    def _update(self, nn, gradient):
+        for var_uid in nn.vars:
+            jv = self.learning_rate * gradient[var_uid]
+            if var_uid in self.m:
+                self.m[var_uid] = self.beta * self.m[var_uid] + (1 - self.beta) * (jv * jv)
+            else:
+                self.m[var_uid] = (1 - self.beta) * (jv * jv)
+
+            nn.vars[var_uid].value += - self.learning_rate * jv / np.sqrt(self.m[var_uid] + self.epsilon)
 
 
 class AdamOptimizer(BaseOptimizer):
-    pass
+    m = {}
+    s = {}
+
+    def __init__(self, loss, learning_rate, beta_1=0.9,  beta_2=0.999, epsilon=1e-10):
+        super().__init__(loss, learning_rate)
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
+        self.n_iter = 1
+
+    def _update(self, nn, gradient):
+        for var_uid in nn.vars:
+            jv = self.learning_rate * gradient[var_uid]
+            if var_uid in self.m:
+                self.m[var_uid] = self.beta_1 * self.m[var_uid] - (1 - self.beta_1) * jv
+            else:
+                self.m[var_uid] = (1 - self.beta_1) * jv
+
+            if var_uid in self.s:
+                self.s[var_uid] = self.beta_2 * self.s[var_uid] + (1 - self.beta_2) * (jv * jv)
+            else:
+                self.s[var_uid] = (1 - self.beta_2) * (jv * jv)
+
+            self.m[var_uid] = self.m[var_uid] / (1 - self.beta_1**self.n_iter)
+            self.s[var_uid] = self.s[var_uid] / (1 - self.beta_2 ** self.n_iter)
+
+            nn.vars[var_uid].value += self.learning_rate * self.m[var_uid] / np.sqrt(self.s[var_uid] + self.epsilon)
+
 
 
 OPTIMIZERS = {
