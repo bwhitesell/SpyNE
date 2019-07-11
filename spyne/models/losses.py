b9b8128d5391b7cd5d753c111b875dd014f33377
bwhitesell <whitesell.ben@gmail.com>
2019-07-11 09:59:19 -0700

    Fixing logloss

--- spyne/models/losses.py
+++ spyne/models/losses.py
@@ -1,5 +1,8 @@
-from spyne.autodiff.operations.arithmetic import TensorSubtraction, TensorElemMultiply
+import numpy as np
+
+from spyne.autodiff.operations.arithmetic import TensorSubtraction, TensorElemMultiply, TensorAddition
 from spyne.autodiff.operations.elements import TensorSquared, TensorSum, TensorNegLog
+from spyne.autodiff.variables.variables import TensorConst
 
 
 def mean_squared_error(y, y_hat):
@@ -9,9 +12,23 @@ def mean_squared_error(y, y_hat):
 
 
 def log_loss(y, y_hat):
-    yh_neg_log = TensorNegLog(y_hat)
-    lp = TensorElemMultiply(yh_neg_log, y)
-    return TensorSum(lp)
+    pos_prob_log = TensorNegLog(y_hat)
+    pos_prob_rect = TensorElemMultiply(pos_prob_log, y)
+
+    neg_prob = TensorSubtraction(
+        TensorConst(np.ones(y_hat.shape)),
+        y_hat,
+    )
+    neg_prob_log = TensorNegLog(neg_prob)
+    neg_outcomes = TensorSubtraction(
+        TensorConst(np.ones(y.shape)),
+        y,
+    )
+    neg_prob_rect = TensorElemMultiply(neg_prob_log, neg_outcomes)
+
+    log_probs = TensorAddition(neg_prob_rect, pos_prob_rect)
+
+    return TensorSum(log_probs)
 
 
 LOSSES = {
