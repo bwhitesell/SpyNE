Fixed gradient calculation bug, one hot encoding bug

spyne/autodiff/operations/activations.py
@@ -44,11 +44,11 @@ class TensorSigmoid(UniTensorOperation):
         return e / (e + 1)
 
     def vector_jacobian_product(self, func=lambda g: g):
-        e = np.exp(self._a)
+        e = np.exp(-1 * self._a)
 
         @nest_func(func)
         def a_vjp(g):
-            return np.multiply(g, np.divide(e, np.square((1 + e))))
+            return np.multiply(g, e / (1 + e)**2)
 
         return a_vjp
 

spyne/autodiff/operations/utils.py
@@ -21,7 +21,7 @@ def one_hot_encode_categorical_target(y):
     n_categories = np.unique(y).size
     n_samples = y.size
     y_adj = np.zeros((n_samples, n_categories))
-    y_adj[np.arange(n_samples), y] = 1
+    y_adj[np.arange(n_samples), y-1] = 1
     return y_adj
 
 

spyne/layers/layers.py
@@ -24,7 +24,7 @@ class FullyConnectedLayer:
         self.products_shape = self._get_product_shape(x)
 
         # xavier initialization
-        r = np.sqrt(6/(sum(self.input_shape) + sum(self.products_shape)))
+        r = np.sqrt(2/(sum(self.input_shape) + sum(self.products_shape)))
         self.w = self._add_var(np.random.uniform(-r, r, self.weights_shape))
         self.b = self._add_var(np.random.random(self.products_shape))
 

spyne/models/optimizers.py
@@ -55,8 +55,8 @@ class BaseOptimizer:
                 self._handle_prints(epoch, batch, n_batches)
             # eval performance
             rfc = LogisticRegression(multi_class='auto', solver='liblinear')
-            rfc.fit(x_train, y_train)
-            rfc_ll = log_loss(y_test, rfc.predict_proba(x_test))
+            rfc.fit(x_train, np.where(y_train == 1)[1])
+            rfc_ll = log_loss(np.where(y_test == 1)[1], rfc.predict_proba(x_test))
             train_loss = self._eval_perf(x_train, y_train, nn)
             validation_loss = self._eval_perf(x_test, y_test, nn)
 